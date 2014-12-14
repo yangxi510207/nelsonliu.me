81070cd66aa848e6983d3eddd9a4bb244a3de5f4
Nelson Liu <nelson.liu.2009@gmail.com>
2014-12-14 01:15:19 -0800

    finished about me section

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 $(document).ready(function(){/* off-canvas sidebar toggle */
 
   $('[data-toggle=offcanvas]').click(function() {
-    $(this).toggleClass('inline-xs text-center');
+    $(this).toggleClass('visible-xs text-center');
     $(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
     $('.row-offcanvas').toggleClass('active');
-    $('#lg-menu').toggleClass('none-xs').toggleClass('inline-xs');
-    $('#xs-menu').toggleClass('inline-xs').toggleClass('none-xs');
+    $('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
+    $('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
     $('#btnShow').toggle();
   });
 
